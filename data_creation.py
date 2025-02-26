ad74627432aaaa7f275642dbcd38f65b49c90beb
YashDeCoder <yashisrani35@gmail.com>
2025-02-26 10:31:24 +0100

    FEAT - Added support to use OpenAI models

--- data_creation.py
+++ data_creation.py
@@ -1,8 +1,10 @@
 import re
 import pandas as pd
-from ollama import chat
 import json
 from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Generate answers from LLM
 # TODO: would be nice to have another function to populate the questions with a larger amount of questions. The same questions but phrased differently to see how consistent these models are.
@@ -20,10 +22,10 @@ def create_variants(model:str, n:int):
                 "content": row['instruction']
             }
         ]
-        
-        output = chat(model=model, messages=messages)
+
+        output = client.chat.completions.create(model=model, messages=messages)
         variants.append({
-            "response":"q_0:"+row['instruction']+"\n\n"+output['message']['content'],
+            "response":"q_0:"+row['instruction']+"\n\n"+output.choices[0].message.content,
         })
     
     with open(f'./data/questions/{model}_variants.json', 'w') as f:
@@ -50,39 +52,28 @@ def generate_response(model:str, data:list):
                     "content": question
                 }]
                 
-            output = chat(model=model, messages=message)
+            output = client.chat.completions.create(model=model, messages=message)
             question_grouped.append({
                 "user_input": question,
-                "response":output['message']['content'],
+                "response":output.choices[0].message.content,
             })
         dataset.append(question_grouped)
 
-    with open(f'./data/{model}_data.json', 'w') as f:
+    with open(f'./data/output/{model}_data.json', 'w') as f:
         json.dump(dataset, f)
 
 # TODO: make this main function or some
+api_key = os.getenv("OPENAI_API_KEY")
 model = "gpt-4o"
 if model.startswith('gpt'):
-    client = OpenAI()
+    client = OpenAI(api_key=api_key)
 else:
     client = OpenAI(
         base_url = 'http://localhost:11434/v1',
         api_key='ollama', # required, but unused
     )
+
 # LOAD DATA
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
-
-print(completion.choices[0].message)
-
 df = pd.read_json("hf://datasets/databricks/databricks-dolly-15k/databricks-dolly-15k.jsonl", lines=True)
 filtered_df = df[df['category'] == 'open_qa']
 df_open_qa = filtered_df.head(10)
